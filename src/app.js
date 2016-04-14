a76d46ed203bcc3ec1a9d8b2218cae4f99dc1f33
Danil Skachkov <danil.skachkov@speaktoit.com>
2016-04-14 16:31:51 +0600

    Add lang parameter

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ const request = require('request');
 
 const REST_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_LANG = process.env.APIAI_LANG || 'en';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 
-const apiAiService = apiai(APIAI_ACCESS_TOKEN, "deprecated", {hostname: "dev.api.ai", endpoint: "/api/"});
+const apiAiService = apiai(APIAI_ACCESS_TOKEN, "deprecated", {hostname: "dev.api.ai", endpoint: "/api/", language: APIAI_LANG});
 const sessionIds = new Map();
 
 function processEvent(event) {
