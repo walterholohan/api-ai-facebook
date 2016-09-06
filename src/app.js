a7a2a116c918870a523812df6ecce95370e9a3a9
Danil Skachkov <danil.skachkov@speaktoit.com>
2016-09-06 11:42:56 +0700

    Send username in context

--- src/app.js
+++ src/app.js
@@ -29,49 +29,60 @@ function processEvent(event) {
         }
 
         console.log("Text", text);
-        
-        userInfoRequest(sender);
 
-        let apiaiRequest = apiAiService.textRequest(text,
-            {
-                sessionId: sessionIds.get(sender)
-            });
-
-        apiaiRequest.on('response', (response) => {
-            if (isDefined(response.result)) {
-                let responseText = response.result.fulfillment.speech;
-                let responseData = response.result.fulfillment.data;
-                let action = response.result.action;
-
-                if (isDefined(responseData) && isDefined(responseData.facebook)) {
-                    try {
-                        console.log('Response as formatted message');
-                        sendFBMessage(sender, responseData.facebook);
-                    } catch (err) {
-                        sendFBMessage(sender, {text: err.message });
-                    }
-                } else if (isDefined(responseText)) {
-                    console.log('Response as text message');
-                    // facebook API limit for text length is 320,
-                    // so we split message if needed
-                    var splittedText = splitResponse(responseText);
-
-                    async.eachSeries(splittedText, (textPart, callback) => {
-                        sendFBMessage(sender, {text: textPart}, callback);
+        userInfoRequest(sender)
+            .then((userInfo)=> {
+                let apiaiRequest = apiAiService.textRequest(text,
+                    {
+                        sessionId: sessionIds.get(sender),
+                        contexts: [
+                            {
+                                name: "generic",
+                                parameters: {
+                                    facebook_user_name: userInfo.first_name
+                                }
+                            }
+                        ]
                     });
-                }
 
-            }
-        });
+                apiaiRequest.on('response', (response) => {
+                    if (isDefined(response.result)) {
+                        let responseText = response.result.fulfillment.speech;
+                        let responseData = response.result.fulfillment.data;
+                        let action = response.result.action;
 
-        apiaiRequest.on('error', (error) => console.error(error));
-        apiaiRequest.end();
+                        if (isDefined(responseData) && isDefined(responseData.facebook)) {
+                            try {
+                                console.log('Response as formatted message');
+                                sendFBMessage(sender, responseData.facebook);
+                            } catch (err) {
+                                sendFBMessage(sender, {text: err.message});
+                            }
+                        } else if (isDefined(responseText)) {
+                            console.log('Response as text message');
+                            // facebook API limit for text length is 320,
+                            // so we split message if needed
+                            var splittedText = splitResponse(responseText);
+
+                            async.eachSeries(splittedText, (textPart, callback) => {
+                                sendFBMessage(sender, {text: textPart}, callback);
+                            });
+                        }
+
+                    }
+                });
+
+                apiaiRequest.on('error', (error) => console.error(error));
+                apiaiRequest.end();
+
+            }).catch(err=> {
+                console.error(err);
+            });
     }
 }
 
 function splitResponse(str) {
-    if (str.length <= 320)
-    {
+    if (str.length <= 320) {
         return [str];
     }
 
@@ -81,31 +92,28 @@ function splitResponse(str) {
 
 }
 
-function chunkString(s, len)
-{
+function chunkString(s, len) {
     var curr = len, prev = 0;
 
     var output = [];
 
-    while(s[curr]) {
-        if(s[curr++] == ' ') {
-            output.push(s.substring(prev,curr));
+    while (s[curr]) {
+        if (s[curr++] == ' ') {
+            output.push(s.substring(prev, curr));
             prev = curr;
             curr += len;
         }
-        else
-        {
+        else {
             var currReverse = curr;
             do {
-                if(s.substring(currReverse - 1, currReverse) == ' ')
-                {
-                    output.push(s.substring(prev,currReverse));
+                if (s.substring(currReverse - 1, currReverse) == ' ') {
+                    output.push(s.substring(prev, currReverse));
                     prev = currReverse;
                     curr = currReverse + len;
                     break;
                 }
                 currReverse--;
-            } while(currReverse > prev)
+            } while (currReverse > prev)
         }
     }
     output.push(s.substr(prev));
@@ -149,17 +157,23 @@ function doSubscribeRequest() {
 }
 
 function userInfoRequest(userId) {
-    request({
-            method: 'GET',
-            uri: "https://graph.facebook.com/v2.6/" + userId + "?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=" + FB_PAGE_ACCESS_TOKEN
-        },
-        function (error, response) {
-            if (error) {
-                console.error('Error while userInfoRequest: ', error);
-            } else {
-                console.log('userInfoRequest result: ', response.body);
-            }
-        })
+    return new Promise((resolve, reject) => {
+        request({
+                method: 'GET',
+                uri: "https://graph.facebook.com/v2.6/" + userId + "?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=" + FB_PAGE_ACCESS_TOKEN
+            },
+            function (error, response) {
+                if (error) {
+                    console.error('Error while userInfoRequest: ', error);
+                    reject(error);
+                } else {
+                    console.log('userInfoRequest result: ', response.body);
+                    resolve(response.body);
+                }
+            });
+    });
+
+
 }
 
 function isDefined(obj) {
@@ -176,12 +190,12 @@ function isDefined(obj) {
 
 const app = express();
 
-app.use(bodyParser.text({ type: 'application/json' }));
+app.use(bodyParser.text({type: 'application/json'}));
 
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
-        
+
         setTimeout(function () {
             doSubscribeRequest();
         }, 3000);
